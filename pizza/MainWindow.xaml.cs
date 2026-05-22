b66566436f83320ad0263d6c1ab2a04774d5b720
232415 <232415@DESKTOP-10MIQUT>
2026-05-22 15:07:14 +0300

    Добавьте файлы проекта.

---
+++ pizza/MainWindow.xaml.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace pizza
+{
+    public partial class MainWindow : Window
+    {
+        private int priceMargarita = 450;
+        private int pricePepperoni = 550;
+        private int priceHawaii = 500;
+        private int priceCola = 100;
+        private int priceJuice = 80;
+        private int priceWater = 50;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private int GetPizzaPrice()
+        {
+            if(MargaritaRbtn.IsChecked == true)
+                return priceMargarita;
+            if (PepperoniRbtn.IsChecked == true)
+                return pricePepperoni;
+            else
+                return priceHawaii;
+        }
+        private string GetPizzaName()
+        {
+            if (MargaritaRbtn.IsChecked == true)
+                return "Маргарита";
+            if (PepperoniRbtn.IsChecked == true)
+                return "Пепперони";
+            else
+                return "Гавайская";
+        }
+        private int GetPizzaCount()
+        {
+            if(int.TryParse(QuantityTB.Text, out int count))
+                return count;
+            return 1;
+        }
+        private int GetDrinkPrice()
+        {
+            if (DrinkChbx.IsChecked != true)
+                return 0;
+            if (ColaRbtn.IsChecked == true) 
+                return priceCola;
+            if (JuiceRbtn.IsChecked == true)
+                return priceJuice;
+            else 
+                return priceWater;
+        }
+        private string GetDringName()
+        {
+            if (DrinkChbx.IsChecked != true)
+                return "не выбран";
+            if (ColaRbtn.IsChecked == true)
+                return "кола";
+            if (JuiceRbtn.IsChecked == true)
+                return "сок";
+            else
+                return "вода";
+        }
+        private void UpdateTotal()
+        {
+            int pizzaPrica = GetPizzaPrice();
+            int kol = GetPizzaCount();
+            int drinkPrice = GetDrinkPrice();
+            int total = (pizzaPrica * kol) + drinkPrice;
+
+            TotalTl.Text = $"Итого: {total} руб.";
+        }
+        private void AnyParameter_Changed(object sender, RoutedEventArgs e)
+        {
+            UpdateTotal();
+        }
+        private void MinusBtn_Click(object sender, RoutedEventArgs e)
+        {
+            int kol = GetPizzaCount();
+            if (kol > 1)
+            {
+                QuantityTB.Text = (kol - 1).ToString();
+            }
+            UpdateTotal();
+        }
+        private void PlusBtn_Click(object sender, RoutedEventArgs e)
+        {
+            int kol = GetPizzaCount();
+            if (kol < 10)
+            {
+                QuantityTB.Text = (kol + 1).ToString();
+            }
+            UpdateTotal();
+        }
+        private void DrinkChbx_Click(object sender, RoutedEventArgs e)
+        {
+            bool isEnable = DrinkChbx.IsChecked == true;
+            DrinkPanel.IsEnabled = isEnable;
+            UpdateTotal();
+        }
+        private void OrderBtn_Click(object sender, RoutedEventArgs e)
+        {
+            string pizzaName = GetPizzaName();
+            int kol = GetPizzaCount();
+            string drinkName = GetDringName();
+            int total = (GetPizzaPrice() * kol) + GetDrinkPrice();
+
+            string message = "ВАШ ЗАКАЗ\n" +
+                            $"Пицца: {pizzaName}, {kol} шт.\n" +
+                            $"Напиток: {drinkName}\n" +
+                            $"Сумма {total} руб.\n" +
+                            $"Спасибо за заказ";
+            MessageBox.Show(message, "Заказ оформлен", MessageBoxButton.OK, MessageBoxImage.Information);
+            ResetAll();
+        }
+        private void ResetBtn_Click(object sender, RoutedEventArgs e)
+        {
+            ResetAll();
+        }
+        private void ResetAll()
+        {
+            MargaritaRbtn.IsChecked = true;
+            QuantityTB.Text = "1";
+            DrinkChbx.IsChecked = false;
+            WaterRbtn.IsChecked = true;
+            DrinkPanel.IsEnabled = false;
+            UpdateTotal();
+        }
+    }
+}
